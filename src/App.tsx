d4eab9a1e19c2cabe739590758daaf8eb3f962d5
nirinasoa <yaelnirinasoa@gmail.com>
2025-05-13 09:05:17 +0300

    Fix unused variable errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="bg-gray-50 p-8 min-h-screen flex justify-center items-center ">
       <article className="bg-white max-w-4xl p-8 rounded-lg shadow-xl">
